2afaf6998913cd4729bab12c3dbf0ea594eebef9
sagarjalp <sagarjalp99@gmail.com>
2026-09-19 07:47:42 +0530

    Add files via upload

---
+++ practice question .py
@@ -0,0 +1,22 @@
+# Create an integer variable  CODE SNIPPET num = 15 and print its data type to the console using the type() function.
+num = 15
+print(type(num))
+
+# Inspect the data type of  CODE SNIPPET x = 12.75 . Then, cast x to an integer using int() and print the truncated output.
+x = 12.75
+result = int(x)
+print(type(x))
+print(result)
+
+# Create a string variable  CODE SNIPPET age = "25" . Convert it into an integer, add 5 to it, and print the computed final value.                       
+age = "25"
+result = int(age) + 5
+print(result)
+
+# Define val = 9 . Cast it to a floating-point number using # CODE SNIPPET float() and verify the decimal output representation.
+val = 9
+result = float(val)
+print(result)
+print(type(result))
+
+
