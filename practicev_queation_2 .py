de1abf78df5771cf725b6ffeb0c486a1d23ef985
sagarjalp <sagarjalp99@gmail.com>
2026-09-19 08:23:42 +0530

    Add files via upload

---
+++ practicev_queation_2 .py
@@ -0,0 +1,29 @@
+ # Create variable x with value 5 # Create variable y with value "John" # Print the type of x
+x = 5
+y = "john"
+print(type(x)) 
+
+# Assign 10 to variable a # Print the type of a # Change a to "Hello" # Print the type of a again
+a = 10 
+print(type(a))
+a = "hello"
+print(type(a))
+
+# Assign values "Red", "Green", "Blue" to variables r, g, b in one line # Print each variable
+r,g,b = "Red", "Green", "Blue"
+print(r)
+print(g)
+print(b)
+
+ # Assign the value 0 to variables x, y, z in one line # Print each variable
+x=y=z = 0
+print(x)
+print(y)
+print(z)
+
+ # Create a list fruits = ["apple", "banana", "cherry"] # Unpack the list into variables 1, 2, 3 # Print each variable
+list_fruits = ["apple", "banana", "charry"]
+fruits1 , fruits2 , fruits3 = list_fruits
+print(fruits1)
+print(fruits2)
+print(fruits3)
